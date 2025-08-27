79360a3dde69f68badcdfdc3bd4b4565d27f1a7b
AhmedZedan <ahmedzedan650@gmail.com>
2025-08-27 19:48:00 +0300

    12 - User Authentication Part 2

--- market/__init__.py
+++ market/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '8cb61608b80eeeaa00f5b9d5'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from market import routes
--- market/routes.py
+++ market/routes.py
@@ -2,6 +2,7 @@ from market import app, db
 from market.models import Item, User
 from flask import render_template, url_for, redirect, flash
 from market.form import RegisterForm, LoginForm
+from flask_login import login_user
 
 @app.route("/")
 @app.route("/home")
@@ -32,4 +33,12 @@ def register_page():
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
     form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(form.password.data):
+            login_user(attempted_user)
+            flash(f'Success! You are logged in as: {attempted_user.username}', category='success')
+            return redirect(url_for('market_page'))
+        else:
+            flash('Invalid username or password', category='danger')
     return render_template('login.html', form=form)
