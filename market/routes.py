f85d9b0aed3c989fc2e2d5c2ee33734013487f79
AhmedZedan <ahmedzedan650@gmail.com>
2025-08-28 15:38:24 +0300

    13 - Logout & Customizations

--- market/routes.py
+++ market/routes.py
@@ -2,7 +2,7 @@ from market import app, db
 from market.models import Item, User
 from flask import render_template, url_for, redirect, flash
 from market.form import RegisterForm, LoginForm
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 
 @app.route("/")
 @app.route("/home")
@@ -10,6 +10,7 @@ def home_page():
     return render_template("home.html")
 
 @app.route("/market")
+@login_required
 def market_page():
     items = Item.query.all()
     return render_template("market.html", items_data=items)
@@ -24,6 +25,8 @@ def register_page():
                              )
         db.session.add(user_to_create)
         db.session.commit()
+        login_user(user_to_create)
+        flash(f"Account created successfully! You are now logged in as {user_to_create.username}", category='success')
         return redirect(url_for('market_page'))
     if form.errors != {}:  # If there are not errors from the validations
         for err_msg in form.errors.values():
@@ -42,3 +45,9 @@ def login_page():
         else:
             flash('Invalid username or password', category='danger')
     return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash('You are logged out', category='info')
+    return redirect(url_for('home_page'))
